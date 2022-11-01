1f991fa57696b524c5381c3db2ad8a5e7b8464c1
jonathan047 <barrosjonathan737@gmail.com>
2022-11-01 19:25:07 -0300

    --- Auto Git Commit ---

--- POO/clinica_medica.py
+++ POO/clinica_medica.py
@@ -3,10 +3,21 @@ condultas = []
 
 class ConsultaMedica:
     def __init__(self,data_consulta,paciente,medico):
-        self.__data_consulta = data_consulta
+        if self.__data_consulta < datetime.today():
+            raise ValueError('Data Antiga!')
+        else:
+            self.__data_consulta = data_consulta
+        
         self.__paciente = paciente
         self.__medico = medico
         self.__pago = False
         self.__valor = 300
         self.__data_retorno = None
         
+@property
+def valor(self):
+    return self.__valor
+
+@valor.setter
+def valor(self,value):
+    senha = input("Entre com a senha: ")
