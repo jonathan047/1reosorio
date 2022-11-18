1c969edc4fa99bdc5a7f703194a7d79173d60dc0
jonathan047 <barrosjonathan737@gmail.com>
2022-11-18 03:42:53 -0300

    --- Auto Git Commit ---

--- POO/Celular.py
+++ POO/Celular.py
@@ -3,15 +3,22 @@ class Celular:
         self.__codigo
         self.__capacidade
         self.__perc_carga
-        
+    @property
+    def codigo(self)    
     def carregar(self,valor1):
         valor = 0
-        if self.__perc_carga <=100:
+        self.__perc_carga = valor
+        if self.__perc_carga <=100 and self.__perc_carga < 0:
+            self.__perc_carga += 1
             return self.__perc_carga
-        elif self.__perc_carga < 0:
-            
         else:
             print("Erro no valor do carregamento!")
     
     def descaregar(self,valor):
-        pass
+        valor = 0
+        self.__perc_carga = valor
+        if self.__perc_carga >= 0 and self.__perc_carga <= 100:
+            self.__perc_carga -=1
+            return self.__perc_carga
+        else:
+            print("Erro no valor de descarregamento!")
